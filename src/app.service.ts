699d889b15510bbffc626a8d25a089f41dc3788e
Rohmat Tri Indra <rohmat.indra@bukalapak.com>
2022-01-30 17:42:25 +0700

    test deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World - AWS';
+    return 'Hello World - The service is running on EKS at the moment';
   }
 }
